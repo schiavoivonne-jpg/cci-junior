7c28121472ba3b2bfbe3d3ea22d53d5a0bfa4936
schiavoivonne-jpg <schiavoivonne@gmail.com>
2026-06-07 20:29:14 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: {
+content: ['./index.html', './*.{js,jsx}'],  theme: {
     extend: {
       colors: {
         cci: {
